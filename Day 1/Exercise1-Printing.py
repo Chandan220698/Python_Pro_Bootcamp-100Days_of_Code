5274e9f955bfc5f07dfd4c708f12bcc86e0a9977
unknown <ck220698@gmail.com>
2022-09-01 23:45:05 +0530

    Day 1

---
+++ Day 1/Exercise1-Printing.py
@@ -0,0 +1,16 @@
+# Program to print following:
+'''
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+'''
+
+# Solution
+
+print('''Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')''')
+
+# or
+# print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+## \n is used to print in next line
---
+++ Day 1/code1.py
@@ -0,0 +1,5 @@
+## Print is used to display the output to console
+print("Hello World !")      # " " is used to tell complier that everything inside this is not code but string.
+                            # String is sequence of characters. 
+
+# print("Hello World !)     #  This will throw an error as it is missing closing quote ` " `.
